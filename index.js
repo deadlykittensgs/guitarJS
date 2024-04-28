7d5275dcb2815ce7361650e42f07a0ef7dabc5e9
deadlykittensgs <161607814+deadlykittensgs@users.noreply.github.com>
2024-04-28 13:26:29 -0500

    file started

---
+++ index.js
@@ -0,0 +1,21 @@
+
+let key = ""
+let notes = []
+
+function getKey() {
+    key = a //the input from the first box 
+
+}
+
+
+
+function getScale(scaleType, Key) {
+    // get the notes in a scale in the 
+    // given key and light them up on the fretboard 
+
+}
+
+
+function WhatKey(notes) {
+    //taker notes selected and return a chord name
+}
