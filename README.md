502284afccdae01d99863ad34f313ca83ef6e562
deadlykittensgs <161607814+deadlykittensgs@users.noreply.github.com>
2024-04-28 14:59:29 -0500

    key finder working

---
+++ README.md
@@ -0,0 +1,9 @@
+goal
+
+facebook  music group users always constantly post pictures of new players with their finger position on the neck asking what chord they are playing. this app will be to help them be able to look up chords easily 
+
+goal make a picture of the fret board that you can click the notes to highlight them
+
+next make a function that takes the highlighted notes and checks them with a database to see what chord would be made out of that variation of notes. if none return chord not found 
+
+secondary objective make a page that shows all the notes in scales in a key 
--- index.js
+++ index.js
@@ -1,21 +1,64 @@
+// define
+const musicNotes = ["A", "A#", "B","C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
 
-let key = ""
+
+
+// get from doc
+let chosenKey = document.getElementById("chosenKey")
+let Submit = document.getElementById("Submit")
+
+// onclick 
+
+let key =  ""
 let notes = []
 
-function getKey() {
-    key = a //the input from the first box 
 
+// functions 
+
+
+function getKey() {
+    inputValue = chosenKey.value;
+    let key = chosenKey.value.toUpperCase()
+    console.log(key)
+    let number = findIndexOfChord(key)
+    console.log(number)
+   let correctedNoteOrder = getScale(number)
+   console.log(correctedNoteOrder)
+
+
+// reset everything 
+    chosenKey.value = ""
+    return key
+}
+function findIndexOfChord(inputValue) {
+    let findKey = inputValue;
+    let index = musicNotes.indexOf(findKey);
+    index = musicNotes.indexOf(findKey);
+if (index !== -1) {
+return(index)
+} else {
+    console.log(findKey, "key Not Found");
+}
 }
 
 
+function getScale(scaleSet) {
+   let keyLocation = scaleSet
+    let orderedArray = musicNotes.slice(keyLocation).concat(musicNotes.slice(0, keyLocation));
 
-function getScale(scaleType, Key) {
+//console.log(orderedArray)
+return  orderedArray
+
+//let correctNotes = []
+    //set the array to start at the [i index of the key] and go through the rest of the notes and make a new array
     // get the notes in a scale in the 
     // given key and light them up on the fretboard 
 
 }
 
 
+
 function WhatKey(notes) {
     //taker notes selected and return a chord name
 }
+
