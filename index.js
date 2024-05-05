7dd5a6268512076029ac5491016aa6567e368001
deadlykittensgs <161607814+deadlykittensgs@users.noreply.github.com>
2024-05-04 20:56:51 -0500

    working need dynamic progression suggestions

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ let nameFour = document.getElementById("iv")
 let nameFive = document.getElementById("v")
 let nameSix = document.getElementById("vi")
 let nameSeven = document.getElementById("vii")
+let clearAndReturn = document.getElementById("clearAndReturn")
 
 let ProgressionsP1 = document.getElementById("ProgressionsP1")
 let ProgressionsP2 = document.getElementById("ProgressionsP2")
@@ -32,28 +33,17 @@ let key =  ""
 let notes = []
 
 
-
 // functions 
 function getKey() {
-   // maybe put an if clause here for the minor catch
     inputValue = chosenKey.value;
     let key = chosenKey.value.toUpperCase()
-    console.log(key)
     let number = findIndexOfChord(key)
-    console.log(number)
-   let correctedNoteOrder = getScale(number)
-// reset everything 
-    // chosenKey.value = ""
+    let correctedNoteOrder = getScale(number)
+
+    // chosenKey.value = ""   if you want to reset value on load
    
     return correctedNoteOrder
 }
-
-// if (key.includes("m")){
-//     ShowNotesInMinorKey(correctedNoteOrder)
-//    }
-//    else {
-//     ShowNotesInMajorKey(correctedNoteOrder)
-//    }
 
 // find the index of the key given
 function findIndexOfChord(inputValue) {
@@ -70,13 +60,12 @@ return(index)
 // take the key and make a list of all the notes in order
 function getScale(scaleSet) {
    let keyLocation = scaleSet
-        let orderedArray = musicNotes.slice(keyLocation).concat(musicNotes.slice(0, keyLocation));
+   let orderedArray = musicNotes.slice(keyLocation).concat(musicNotes.slice(0, keyLocation));
 return  orderedArray
 }
 
-// major works
 function ShowNotesInMajorKey() {
-
+   clearBoard ()
     index = getKey()
     showHide()
     highlightNotes(index[0],index[2], index[4],index[5],index[7],index[9],index[11])
@@ -95,15 +84,16 @@ function ShowNotesInMajorKey() {
    nameFive.innerText = "5"
    nameSix.innerText = "6"
    nameSeven.innerText = "7"
-   chordProgressionsInKey(index)
+   
    return index[0,2,4,5,7,9,11]
 } 
 
 
 function ShowNotesInMinorKey(index) {
-
+   clearBoard ()
     index = getKey()
     showHide()
+    highlightNotes(index[0],index[2],index[3],index[5],index[7],index[8],index[10])
 
     one.innerText = index[0]
     two.innerText = index[2]
@@ -125,11 +115,67 @@ function ShowNotesInMinorKey(index) {
      
  } 
 
- function ShowNotesInPentatonic() {
+ function ShowChord() {
+   clearBoard ()
+   index = getKey()
+   showHide()
+   highlightNotes(index[0],)
+   highlightNotesRed( index[4])
+   highlightNotesGreen(index[7])
 
+   one.innerText = index[0]
+   two.innerText = index[4]
+   three.innerText = index[7]
+   four.innerText = ""
+   five.innerText = ""
+   six.innerText = ""
+   seven.innerText = ""
+
+   nameOne.innerText ="R"
+   nameTwo.innerText ="3"
+   nameThree.innerText ="5"
+   nameFour.innerText =""
+   nameFive.innerText =""
+   nameSix.innerText =""
+   nameSeven.innerText =""
+
+   return index
+
+ }
+
+ function ShowChordMinor() {
+   clearBoard ()
+   index = getKey()
+   showHide()
+   highlightNotes(index[0],)
+   highlightNotesRed( index[2])
+   highlightNotesGreen(index[7])
+
+   one.innerText = index[0]
+   two.innerText = index[3]
+   three.innerText = index[7]
+   four.innerText = ""
+   five.innerText = ""
+   six.innerText = ""
+   seven.innerText = ""
+
+   nameOne.innerText ="R"
+   nameTwo.innerText ="b3"
+   nameThree.innerText ="5"
+   nameFour.innerText =""
+   nameFive.innerText =""
+   nameSix.innerText =""
+   nameSeven.innerText =""
+
+   return index
+
+ }
+
+ function ShowNotesInPentatonic() {
+   clearBoard ()
     index = getKey()
     showHide()
-
+    highlightNotes(index[0],index[2],index[4],index[7],index[9])
     one.innerText = index[0]
     two.innerText = index[2]
     three.innerText = index[4]
@@ -146,15 +192,15 @@ function ShowNotesInMinorKey(index) {
     nameSix.innerText = "-"
     nameSeven.innerText ="-"
 
-    chordProgressionsInKey(index)
     return index
 
  }
 
  function ShowNotesInPentatonicMinor() {
-
+   clearBoard ()
     index = getKey()
     showHide()
+    highlightNotes(index[0],index[3],index[5],index[7],index[10])
 
     one.innerText = index[0]
     two.innerText = index[3]
@@ -166,28 +212,29 @@ function ShowNotesInMinorKey(index) {
 
     nameOne.innerText ="R"
     nameTwo.innerText = "b3"
-    nameThree.innerText ="P4"
-    nameFour.innerText ="P5"
+    nameThree.innerText ="4"
+    nameFour.innerText ="5"
     nameFive.innerText ="b7"
     nameSix.innerText ="-"
     nameSeven.innerText ="-"
 
-    return
+    return index
    
  }
 
 
  function ShowNotesInBlues() {
-
+   clearBoard ()
     index = getKey()
     showHide()
+    highlightNotes(index[0],index[2],index[3],index[4],index[7],index[9])
 
     one.innerText = index[0]
-    two.innerText = index[3]
-    three.innerText = index[5]
-    four.innerText = index[6]
+    two.innerText = index[2]
+    three.innerText = index[3]
+    four.innerText = index[4]
     five.innerText = index[7]
-    six.innerText = index[10]
+    six.innerText = index[9]
     seven.innerText = "-"
 
 
@@ -198,15 +245,16 @@ function ShowNotesInMinorKey(index) {
     nameFive.innerText ="5"
     nameSix.innerText ="6"
     nameSeven.innerText ="-"
-    chordProgressionsInKey(index)
+
     return index
  }
 
 
  function ShowNotesInBluesMinor() {
-
+   clearBoard ()
     index = getKey()
     showHide()
+    highlightNotes(index[0],index[3],index[5],index[6],index[7],index[10])
 
     one.innerText = index[0]
     two.innerText = index[3]
@@ -227,15 +275,112 @@ function ShowNotesInMinorKey(index) {
      return index
  }
 
+
+// light up correct notes on the dynamic Fret Board
+function highlightNotes(n1,n2,n3,n4,n5,n6,n7,) {
+
+   // try catch for the scales without 7 notes
+    try {
+    let removeHighlight = document.querySelectorAll(".glow")
+ removeHighlight.forEach(removeHighlight => {
+    removeHighlight.classList.remove("glow")
+ })
+ 
+   let new1 = n1.replaceAll("#","S")
+    let highlight = document.querySelectorAll(`.${new1}`)
+    highlight.forEach(highlight =>{
+    highlight.classList.add("glow"); 
+  })
+ 
+ let new2 = n2.replaceAll("#","S")
+ let highlightTwo = document.querySelectorAll(`.${new2}`)
+ highlightTwo.forEach(highlightTwo =>{
+ highlightTwo.classList.add("glow"); 
+ })
+ 
+ let new3 = n3.replaceAll("#","S")
+ let highlightThree = document.querySelectorAll(`.${new3}`)
+ highlightThree.forEach(highlightThree =>{
+ highlightThree.classList.add("glow"); 
+ })
+ 
+ let new4 = n4.replaceAll("#","S")
+ let highlightFour = document.querySelectorAll(`.${new4}`)
+ highlightFour.forEach(highlightFour =>{
+ highlightFour.classList.add("glow"); 
+ })
+ 
+ let new5 = n5.replaceAll("#","S")
+ let highlightFive= document.querySelectorAll(`.${new5}`)
+ highlightFive.forEach(highlightFive =>{
+ highlightFive.classList.add("glow"); 
+ })
+ 
+ let new6 = n6.replaceAll("#","S")
+ let highlightSix = document.querySelectorAll(`.${new6}`)
+ highlightSix.forEach(highlightSix =>{
+ highlightSix.classList.add("glow"); 
+ })
+ 
+ let new7 = n7.replaceAll("#","S")
+ let highlightSeven = document.querySelectorAll(`.${new7}`)
+ highlightSeven.forEach(highlightSeven =>{
+ highlightSeven.classList.add("glow"); 
+ })
+ } catch (error) {
+    console.log("there was an error loading all notes on the fretboard")
+       
+ }
+ }
+
+ function highlightNotesRed(note) {
+
+   // try catch for the scales without 7 notes
+    try {
+    let removeHighlight = document.querySelectorAll(".glowred")
+ removeHighlight.forEach(removeHighlight => {
+    removeHighlight.classList.remove("glowred")
+ })
+ 
+   let new1 = note.replaceAll("#","S")
+    let highlight = document.querySelectorAll(`.${new1}`)
+    highlight.forEach(highlight =>{
+    highlight.classList.add("glowred"); 
+  })
+ 
+ } catch (error) {
+    console.log("there was an error loading all notes on the fretboard")
+       
+ }
+ }
+
+ function highlightNotesGreen(note) {
+
+   // try catch for the scales without 7 notes
+    try {
+    let removeHighlight = document.querySelectorAll(".glowgreen")
+ removeHighlight.forEach(removeHighlight => {
+    removeHighlight.classList.remove("glowgreen")
+ })
+ 
+   let new1 = note.replaceAll("#","S")
+    let highlight = document.querySelectorAll(`.${new1}`)
+    highlight.forEach(highlight =>{
+    highlight.classList.add("glowgreen"); 
+  })
+ 
+ } catch (error) {
+    console.log("there was an error loading all notes on the fretboard")
+       
+ }
+ }
+ 
  function refreshPage() {
     location.reload()
 
  }
 
-function showHide() {
-   answerHide.className = "none"
-}
-
+/// show suggested chord progressions 
 function chordProgressionsInKey(notesForChords) {
    let orderedArrayOfNotes = notesForChords
    let value0 = orderedArrayOfNotes[0]
@@ -250,11 +395,80 @@ function chordProgressionsInKey(notesForChords) {
    let value9 = orderedArrayOfNotes[9]
    let value10 = orderedArrayOfNotes[10]
    let value11 = orderedArrayOfNotes[11]
+
    ProgressionsP1.innerText = `${value0}-${value5}-${value7}-${value0}`
    ProgressionsP2.innerText =  `${value0}-${value9}-${value5}-${value7}`
    ProgressionsP3.innerText = `${value2}-${value7}-${value0}`
    ProgressionsP4.innerText = `${value0}-${value9}-${value2}-${value7}`
 }
+
+
+
+function closeONE() {
+   let fretboardStuff = document.getElementById("fretboard")
+   console.log("hit")
+   fretboardStuff.className = "remove"
+   clearAndReturn.innerText="Return Fretboard/Clear"
+   return
+}
+
+
+// random note Generator 
+
+// Get the reference to the <div> element
+const myBox = document.getElementById('myBox');
+
+// Function to update the text inside the box
+function updateText() {
+   let randomIndex = Math.floor(Math.random() * 11) 
+    myBox.textContent =  musicNotes[randomIndex];
+  
+}
+
+// Update the text every second
+setInterval(updateText, 1000);
+
+// function speedOfDisplay(number) {
+//    speed =  speed  +  number
+//    return  
+// }
+
+
+
+// hide the random note Generator
+function hideRandom() {
+   myBox.className = "hide"
+}
+
+
+//hide the dynamic chord progressions until they are named
+function showHide() {
+   answerHide.className = "none"
+}
+
+
+// show the random note Generator
+function showRandom()  {
+   myBox.className = "none"
+   
+}
+
+function clearBoard () {
+   let removeHighlight = document.querySelectorAll(".glow")
+   let removeHighlightRed = document.querySelectorAll(".glowred")
+   let removeHighlightGreen = document.querySelectorAll(".glowgreen")
+   removeHighlight.forEach(removeHighlight => {
+      removeHighlight.classList.remove("glow")
+   })
+   removeHighlightRed.forEach(removeHighlight => {
+      removeHighlight.classList.remove("glowred")
+   })
+   removeHighlightGreen.forEach(removeHighlight => {
+      removeHighlight.classList.remove("glowgreen")
+   })
+}
+
+
 
 // function chordProgressionsInKeyMinor(notesForChords) {
 //    let orderedArrayOfNotes = notesForChords
@@ -275,116 +489,3 @@ function chordProgressionsInKey(notesForChords) {
 //    ProgressionsP3.innerText = `${value2}-${value7}-${value0}`
 //    ProgressionsP4.innerText = `${value0}-${value9}-${value2}-${value7}`
 // }
-
-
-
-// Get the reference to the <div> element
-const myBox = document.getElementById('myBox');
-
-
-// Function to update the text inside the box
-function updateText() {
-   let randomIndex = Math.floor(Math.random() * 11) 
-    myBox.textContent =  musicNotes[randomIndex];
-  
-}
-
-
-
-// Update the text every second
-setInterval(updateText, 1000);
-
-function speedOfDisplay(number) {
-   speed =  speed  +  number
-   return  
-}
-
-function hideRandom() {
-   myBox.className = "hide"
-}
-function showRandom()  {
-   myBox.className = "none"
-   
-}
-
-
-function highlightNotes(n1,n2,n3,n4,n5,n6,n7,) {
-
-   let removeHighlight = document.querySelectorAll(".glow")
-removeHighlight.forEach(removeHighlight => {
-   removeHighlight.classList.remove("glow")
-})
-
-  let new1 = n1.replaceAll("#","S")
-   let highlight = document.querySelectorAll(`.${new1}`)
-   highlight.forEach(highlight =>{
-   highlight.classList.add("glow"); 
- })
-
-
-let new2 = n2.replaceAll("#","S")
-let highlightTwo = document.querySelectorAll(`.${new2}`)
-highlightTwo.forEach(highlightTwo =>{
-highlightTwo.classList.add("glow"); 
-})
-
-let new3 = n3.replaceAll("#","S")
-let highlightThree = document.querySelectorAll(`.${new3}`)
-highlightThree.forEach(highlightThree =>{
-highlightThree.classList.add("glow"); 
-})
-let new4 = n4.replaceAll("#","S")
-let highlightFour = document.querySelectorAll(`.${new4}`)
-highlightFour.forEach(highlightFour =>{
-highlightFour.classList.add("glow"); 
-})
-let new5 = n5.replaceAll("#","S")
-let highlightFive= document.querySelectorAll(`.${new5}`)
-highlightFive.forEach(highlightFive =>{
-highlightFive.classList.add("glow"); 
-})
-let new6 = n6.replaceAll("#","S")
-let highlightSix = document.querySelectorAll(`.${new6}`)
-highlightSix.forEach(highlightSix =>{
-highlightSix.classList.add("glow"); 
-})
-let new7 = n7.replaceAll("#","S")
-let highlightSeven = document.querySelectorAll(`.${new7}`)
-highlightSeven.forEach(highlightSeven =>{
-highlightSeven.classList.add("glow"); 
-})
-
-}
-//  let highlightTwo = document.querySelectorAll(`.${n2}`)
-//  let highlightThree = document.querySelectorAll(`.${n3}`)
-//  let highlightFour = document.querySelectorAll(`.${n4}`)
-//  let highlightFive = document.querySelectorAll(`.${n5}`,)
-//  let highlightSix = document.querySelectorAll(`.${n6}`)
-//  let highlightSeven = document.querySelectorAll(`.${n7}`)
-
-
-
-//  highlightTwo.forEach(highlight =>{
-//    highlight.classList.add("glow"); 
-//  })
-//  highlightThree.forEach(highlight =>{
-//    highlight.classList.add("glow"); 
-//  })
-//  highlightFour.forEach(highlight =>{
-//    highlight.classList.add("glow"); 
-//  })
-//  highlightFive.forEach(highlight =>{
-//    highlight.classList.add("glow"); 
-//  })
-//  highlightSix.forEach(highlight =>{
-//    highlight.classList.add("glow"); 
-//  })
-//  highlightSeven.forEach(highlight =>{
-//    highlight.classList.add("glow"); 
-//  })
-
-
-
-
-
-
